b2737d77ed92b70c79489ccf47c4d39cf4a4dfeb
d.escalada@alumnos.upm.es <d.escalada@alumnos.upm.es>
2021-04-26 19:02:05 +0200

    Cambiado el color de la caja para que se vea mejor

--- src/Caja.cpp
+++ src/Caja.cpp
@@ -6,25 +6,25 @@ Caja::Caja()
 	suelo.limite2.x = 10.0f;
 	suelo.limite1.y = suelo.limite2.y = 0;
 	suelo.rojo = suelo.azul = 0;
-	suelo.verde = 255;
+	suelo.verde = 100;
 
 	techo.limite1.x = -10.0f;
 	techo.limite2.x = 10.0f;
 	techo.limite1.y = techo.limite2.y = 15.0f;
 	techo.rojo = techo.azul = 0;
-	techo.verde = 255;
+	techo.verde = 100;
 
 	pared_dcha.limite1.x = pared_dcha.limite2.x = 10.0f;
 	pared_dcha.limite1.y = 0;
 	pared_dcha.limite2.y = 15.0f;
 	pared_dcha.rojo = pared_dcha.azul = 0;
-	pared_dcha.verde = 100;
+	pared_dcha.verde = 255;
 
 	pared_izq.limite1.x = pared_izq.limite2.x = -10.0f;
 	pared_izq.limite1.y = 0;
 	pared_izq.limite2.y = 15.0f;
 	pared_izq.rojo = pared_izq.azul = 0;
-	pared_izq.verde = 100;
+	pared_izq.verde = 255;
 
 
 }
