420033086da23b5b0a9f73215ed21daa047d79fe
d.escalada@alumnos.upm.es <d.escalada@alumnos.upm.es>
2021-05-11 12:56:47 +0200

    Añadido sprite muñeco

--- src/Hombre.cpp
+++ src/Hombre.cpp
@@ -5,22 +5,39 @@ Hombre::Hombre()
 {
 	altura = 1.8f;
 	aceleracion.y = aceleracion.x = velocidad.y = 0;
+	sprite.setCenter(1,0);
+	sprite.setSize(2,2);
 }
 
 void Hombre::dibuja() {
-
+    /*
 	glPushMatrix();
 	glTranslatef(posicion.x, posicion.y, 0);
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glutSolidSphere(altura, 20, 20);
 	glPopMatrix();
+	*/
 
+	glPushMatrix();
+	glTranslatef(posicion.x, posicion.y, 1);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	//glutSolidSphere(altura, 20, 20);
+	//gestion de direccion y animacion
+	if (velocidad.x > 0.01)sprite.flip(false, false);
+	if (velocidad.x < -0.01)sprite.flip(true, false);
+	if ((velocidad.x < 0.01) && (velocidad.x > -0.01))
+		sprite.setState(0);
+	else if (sprite.getState() == 0)
+		sprite.setState(1, false);
+	sprite.draw();
+	glPopMatrix();
 }
 
 void Hombre::mueve(float t) {
 
 	posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
 	velocidad = velocidad + aceleracion * t;
+	sprite.loop();
 
 }
 
--- src/Hombre.h
+++ src/Hombre.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Vector2D.h"
+#include "ETSIDI.h"
+using ETSIDI::SpriteSequence;
 class Hombre
 {
 	friend class Interaccion;
@@ -8,11 +10,11 @@ public:
 	void dibuja();
 	void mueve(float t);
 	void setVel(float vx, float vy);
-	
+
 private:
 	float altura;
 	Vector2D posicion, velocidad, aceleracion;
-
+	SpriteSequence sprite{"imagenes/pangPlayer.png", 5};
 
 };
 
